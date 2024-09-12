b32881c8f13d84c3dc3dbef6e7371d9275e2d496
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2024-09-12 17:37:59 +0900

    #2 feat : 진료 스케줄 예약 기능 구현

---
+++ reservation/src/main/java/com/padaks/todaktodak/common/dto/DtoMapper.java
@@ -0,0 +1,16 @@
+package com.padaks.todaktodak.common.dto;
+
+import com.padaks.todaktodak.reservation.domain.Reservation;
+import com.padaks.todaktodak.reservation.dto.ReservationSaveReqDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+// Mapper 어노테이션을 붙이면 MapStruct 가 자동으로 DtoMapper 구현체를 생성해준다.
+@Mapper(componentModel = "spring")
+public interface DtoMapper {
+//    매퍼 클래스에서 DtoMapper 를 찾을수 있도록 하는 코드.
+//    instance를 생성해 주어야 매퍼에 대한 접근이 가능.
+    DtoMapper INSTANCE = Mappers.getMapper(DtoMapper.class);
+
+    Reservation toReservation(ReservationSaveReqDto reservationSaveReqDto);
+}
