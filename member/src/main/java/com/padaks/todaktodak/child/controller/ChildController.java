b438b88d02372c40abb8e354448ccaab55aaaaf0
qkdrmawll <89395493+qkdrmawll@users.noreply.github.com>
2024-09-15 22:15:12 +0900

    feat: 자녀 등록 기능 구현

--- member/src/main/java/com/padaks/todaktodak/child/controller/ChildController.java
+++ member/src/main/java/com/padaks/todaktodak/child/controller/ChildController.java
@@ -1,11 +1,25 @@
 package com.padaks.todaktodak.child.controller;
 
+import com.padaks.todaktodak.child.dto.ChildRegisterReqDto;
+import com.padaks.todaktodak.child.service.ChildService;
+import com.padaks.todaktodak.common.dto.CommonResDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 public class ChildController {
+    private final ChildService childService;
+    @PostMapping("/child")
+    public ResponseEntity<CommonResDto> registerChild(@RequestBody ChildRegisterReqDto dto){
+        childService.createChild(dto.getName(), dto.getSsn());
+        return new ResponseEntity<>(new CommonResDto(HttpStatus.CREATED,"자녀등록 성공",null),HttpStatus.CREATED);
+    }
 }
--- member/src/main/java/com/padaks/todaktodak/child/service/ChildService.java
+++ member/src/main/java/com/padaks/todaktodak/child/service/ChildService.java
@@ -1,13 +1,36 @@
 package com.padaks.todaktodak.child.service;
 
+import com.padaks.todaktodak.child.domain.Child;
+import com.padaks.todaktodak.child.repository.ChildRepository;
+import com.padaks.todaktodak.childparentsrelationship.service.ChildParentsRelationshipService;
+import com.padaks.todaktodak.member.domain.Member;
+import com.padaks.todaktodak.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityNotFoundException;
 
 @Service
 @Slf4j
 @Transactional
 @RequiredArgsConstructor
 public class ChildService {
+    private final ChildRepository childRepository;
+    private final MemberRepository memberRepository;
+    private final ChildParentsRelationshipService childParentsRelationshipService;
+
+    public void createChild(String name, String ssn) {
+        Child child = Child.builder()
+                .name(name)
+                .ssn(ssn)
+                .build();
+        childRepository.save(child);
+        String email = SecurityContextHolder.getContext().getAuthentication().getName();
+        Member member = memberRepository.findByMemberEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException("존재하지 않는 회원입니다"));
+        childParentsRelationshipService.createRelationship(child, member);
+    }
 }
