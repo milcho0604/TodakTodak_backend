251405468fd205aff2b513d92fd2e6eb56bbfb8b
qkdrmawll <89395493+qkdrmawll@users.noreply.github.com>
2024-09-19 17:31:05 +0900

    fix: MedicalChartRepository 의존성 오타 수정

--- reservation/src/main/java/com/padaks/todaktodak/medicalchart/repository/MedicalChartRepository.java
+++ reservation/src/main/java/com/padaks/todaktodak/medicalchart/repository/MedicalChartRepository.java
@@ -1,6 +1,7 @@
 package com.padaks.todaktodak.medicalchart.repository;
 
-import com.padaks.todaktodak.chatmessage.domain.ChatMessage;
+import com.padaks.todaktodak.medicalchart.domain.MedicalChart;
+import com.padaks.todaktodak.reservation.domain.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
