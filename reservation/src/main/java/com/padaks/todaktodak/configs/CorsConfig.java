7f613980ce7cba6c25fd6c9a7a85299c8f270c0a
qkdrmawll <89395493+qkdrmawll@users.noreply.github.com>
2024-09-22 17:32:25 +0900

    fix: 설정 추가

--- reservation/src/main/java/com/padaks/todaktodak/configs/CorsConfig.java
+++ reservation/src/main/java/com/padaks/todaktodak/configs/CorsConfig.java
@@ -9,7 +9,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8081")
+//                .allowedOrigins("http://localhost:8081")
                 .allowedOrigins("https://rtc-test-one.vercel.app")
                 .allowedMethods("*")
                 .allowedHeaders("*")
