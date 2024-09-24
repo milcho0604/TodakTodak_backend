8e63e346a80cf1291a14fed0c17e7228148b4f92
haah21 <55376327+haah21@users.noreply.github.com>
2024-09-25 00:57:14 +0900

    feat:의사 근무시간 생성

--- member/src/main/java/com/padaks/todaktodak/doctoroperatinghours/controller/DoctorOperatingHoursController.java
+++ member/src/main/java/com/padaks/todaktodak/doctoroperatinghours/controller/DoctorOperatingHoursController.java
@@ -1,9 +1,32 @@
 package com.padaks.todaktodak.doctoroperatinghours.controller;
 
+import com.padaks.todaktodak.common.dto.CommonErrorDto;
+import com.padaks.todaktodak.common.dto.CommonResDto;
+import com.padaks.todaktodak.doctoroperatinghours.dto.DoctorOperatingHoursReqDto;
+import com.padaks.todaktodak.doctoroperatinghours.service.DoctorOperatingHoursService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/doctor-operating-hours")
 public class DoctorOperatingHoursController {
+    private final DoctorOperatingHoursService doctorOperatingHoursService;
+
+    @PostMapping("/register/{doctorId}")
+    public ResponseEntity<?> addOperatingHours(@PathVariable Long doctorId, @RequestBody List<DoctorOperatingHoursReqDto> dto){
+        try {
+            doctorOperatingHoursService.addOperatingHours(doctorId, dto);
+            CommonResDto commonResDto = new CommonResDto(HttpStatus.OK, "의사 근무 시간 등록 성공", null);
+            return new ResponseEntity<>(commonResDto, HttpStatus.OK);
+        }catch (IllegalArgumentException e){
+            e.printStackTrace();
+            CommonErrorDto commonErrorDto = new CommonErrorDto(HttpStatus.BAD_REQUEST, e.getMessage());
+            return new ResponseEntity<>(commonErrorDto, HttpStatus.BAD_REQUEST);
+        }
+    }
 }
--- member/src/main/java/com/padaks/todaktodak/doctoroperatinghours/service/DoctorOperatingHoursService.java
+++ member/src/main/java/com/padaks/todaktodak/doctoroperatinghours/service/DoctorOperatingHoursService.java
@@ -1,14 +1,31 @@
 package com.padaks.todaktodak.doctoroperatinghours.service;
 
+import com.padaks.todaktodak.doctoroperatinghours.domain.DoctorOperatingHours;
+import com.padaks.todaktodak.doctoroperatinghours.dto.DoctorOperatingHoursReqDto;
+import com.padaks.todaktodak.doctoroperatinghours.repository.DoctorOperatingHoursRepository;
+import com.padaks.todaktodak.member.domain.Member;
+import com.padaks.todaktodak.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Slf4j
 @Transactional
 @RequiredArgsConstructor
 public class DoctorOperatingHoursService {
+    private final DoctorOperatingHoursRepository doctorOperatingHoursRepository;
+    private final MemberRepository memberRepository;
 
+    public void addOperatingHours(Long doctorId, List<DoctorOperatingHoursReqDto> dtos){
+        Member doctor = memberRepository.findByIdOrThrow(doctorId);
+        for (DoctorOperatingHoursReqDto dto : dtos){
+            DoctorOperatingHours operatingHours = DoctorOperatingHoursReqDto.toEntity(doctor, dto);
+            doctorOperatingHoursRepository.save(operatingHours);
+        }
+    }
 }
