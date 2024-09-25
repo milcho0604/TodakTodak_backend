b01a91f116208863e8e6058cb3441dffc75d4681
qkdrmawll <89395493+qkdrmawll@users.noreply.github.com>
2024-09-25 15:28:23 +0900

    fix: 예약 서비스에 알림 삭제

--- reservation/src/main/java/com/padaks/todaktodak/reservationnotification/repository/NotificationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.padaks.todaktodak.reservationnotification.repository;
-
-import com.padaks.todaktodak.reservationnotification.domain.Notification;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotificationRepository extends JpaRepository<Notification, Long> {
-}
