d231df8e8b4277a6124a5c3671e5ce6f3df15575
wisdom0405 <wisdom0405@kakao.com>
2024-10-07 17:21:15 +0900

    fix : 의사영업시간엔티티, dto 관련 충돌해결

--- member/src/main/java/com/padaks/todaktodak/doctoroperatinghours/domain/DoctorOperatingHours.java
+++ member/src/main/java/com/padaks/todaktodak/doctoroperatinghours/domain/DoctorOperatingHours.java
@@ -26,17 +26,17 @@ public class DoctorOperatingHours extends BaseTimeEntity {
     @OneToOne
     @JoinColumn(name = "member_id")
     private Member member;
+    @Enumerated(EnumType.STRING)
     private DayOfHoliday dayOfWeek;
     private LocalTime openTime;
     private LocalTime closeTime;
-    @Enumerated(EnumType.STRING)
-    private DayOfHoliday untack;
+    private Boolean untact;
 
     public void updateOperatingHours(DoctorOperatingHoursReqDto dto){
-        this.member = dto.getMember();
+        //this.member = dto.getMember();
         this.dayOfWeek = dto.getDayOfWeek();
         this.openTime = dto.getOpenTime();
         this.closeTime = dto.getCloseTime();
-        this.untack = dto.getUntack();
+        this.untact = dto.getUntact();
     }
 }
