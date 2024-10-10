c183ebf7adcf255e984376c765f51a7c37153326
milcho0604 <milcho0604@gmail.com>
2024-10-10 16:33:38 +0900

    chore: 멤버 로그인 필드 무시

--- member/src/main/java/com/padaks/todaktodak/member/dto/MemberLoginDto.java
+++ member/src/main/java/com/padaks/todaktodak/member/dto/MemberLoginDto.java
@@ -1,5 +1,6 @@
 package com.padaks.todaktodak.member.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -9,6 +10,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@JsonIgnoreProperties(ignoreUnknown = true)  // 정의되지 않은 필드는 무시
 public class MemberLoginDto {
     private String memberEmail;
     private String password;
