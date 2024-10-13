871281266dfebde1dfe46fc631436ef0a677df80
milcho0604 <milcho0604@gmail.com>
2024-10-13 18:33:34 +0900

    feat: 캘린더 컨트롤러 수정

--- member/src/main/java/com/padaks/todaktodak/event/controller/EventController.java
+++ member/src/main/java/com/padaks/todaktodak/event/controller/EventController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,36 +23,36 @@ public class EventController {
 
     // 캘린더 이벤트 생성
     @PostMapping("/create")
-    public ResponseEntity<Void> createEvent(@RequestBody EventCreateReqDto dto) {
+    public ResponseEntity<?> createEvent(@RequestBody EventCreateReqDto dto) {
         eventService.createEvent(dto);
-        return ResponseEntity.ok().build();
+        return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
     // 캘린더 이벤트 업데이트
     @PutMapping("/update/{id}")
-    public ResponseEntity<Void> updateEvent(@PathVariable Long id, @RequestBody EventUpdateReqDto dto) {
+    public ResponseEntity<?> updateEvent(@PathVariable Long id, @RequestBody EventUpdateReqDto dto) {
         eventService.UpdateEvent(id, dto);
-        return ResponseEntity.ok().build();
+        return new ResponseEntity<>(dto, HttpStatus.OK);
     }
 
     // 자신의 캘린더 이벤트 리스트 조회
     @GetMapping("/list")
-    public ResponseEntity<Page<EventListResDto>> eventList(Pageable pageable) {
+    public ResponseEntity<Page<?>> eventList(Pageable pageable) {
         Page<EventListResDto> eventList = eventService.eventListResDtos(pageable);
         return ResponseEntity.ok(eventList);
     }
 
     // 특정 이벤트 디테일 조회
     @GetMapping("/detail/{id}")
-    public ResponseEntity<EventDetailResDto> eventDetail(@PathVariable Long id) {
+    public ResponseEntity<?> eventDetail(@PathVariable Long id) {
         EventDetailResDto eventDetail = eventService.eventDetail(id);
         return ResponseEntity.ok(eventDetail);
     }
 
     // 캘린더 이벤트 삭제 (soft delete)
     @DeleteMapping("/delete/{id}")
-    public ResponseEntity<Void> deleteEvent(@PathVariable Long id) {
-        eventService.deletedEvent(id);
-        return ResponseEntity.ok().build();
+    public ResponseEntity<?> deleteEvent(@PathVariable Long id) {
+        eventService.deletedEvent(id);\
+        return new ResponseEntity<>(id, HttpStatus.OK);
     }
 }
