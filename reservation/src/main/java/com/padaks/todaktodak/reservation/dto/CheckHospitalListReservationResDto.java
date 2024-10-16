c36faa555156a48062dee0ffa7028b7afb1872f9
qkdrmawll <89395493+qkdrmawll@users.noreply.github.com>
2024-10-16 10:06:44 +0900

    fix: res dto 예약 id 추가

--- reservation/src/main/java/com/padaks/todaktodak/reservation/dto/CheckHospitalListReservationResDto.java
+++ reservation/src/main/java/com/padaks/todaktodak/reservation/dto/CheckHospitalListReservationResDto.java
@@ -14,6 +14,7 @@ import java.time.LocalTime;
 @AllArgsConstructor
 @Data
 public class CheckHospitalListReservationResDto {
+    private Long id;
     private String doctorName;
     private String doctorEmail;
     private String memberName;
