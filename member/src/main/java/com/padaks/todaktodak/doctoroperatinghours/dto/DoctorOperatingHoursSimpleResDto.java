b6611ed36639e36c1092aa5736ac285b4b3c2b48
qkdrmawll <89395493+qkdrmawll@users.noreply.github.com>
2024-10-28 10:37:35 +0900

    fix: 근무시간 데이터 형식 변경

--- member/src/main/java/com/padaks/todaktodak/doctoroperatinghours/dto/DoctorOperatingHoursSimpleResDto.java
+++ member/src/main/java/com/padaks/todaktodak/doctoroperatinghours/dto/DoctorOperatingHoursSimpleResDto.java
@@ -1,5 +1,6 @@
 package com.padaks.todaktodak.doctoroperatinghours.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.padaks.todaktodak.common.enumdir.DayOfHoliday;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -16,7 +17,9 @@ public class DoctorOperatingHoursSimpleResDto {
     private Long id;
     private String doctorName;
     private DayOfHoliday dayOfWeek;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     private LocalTime openTime;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "HH:mm")
     private LocalTime closeTime;
     private boolean untact;
 }
