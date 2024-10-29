01ecd6c930f20d95a0c505cf91d453dd62df47cd
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2024-10-29 18:51:39 +0900

    TEST : 배포 테스트 16차

--- reservation/src/main/java/com/padaks/todaktodak/ReservationApplication.java
+++ reservation/src/main/java/com/padaks/todaktodak/ReservationApplication.java
@@ -2,13 +2,13 @@ package com.padaks.todaktodak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @EnableFeignClients
 @EnableScheduling
 @EnableWebSocket
