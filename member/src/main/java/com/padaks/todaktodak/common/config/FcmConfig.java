3aa755be6094e3048ab55327ae8919937c1d6761
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2024-10-29 16:20:21 +0900

    TEST : 배포 테스트 6차

--- member/src/main/java/com/padaks/todaktodak/common/config/FcmConfig.java
+++ member/src/main/java/com/padaks/todaktodak/common/config/FcmConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import javax.annotation.PostConstruct;
+import java.io.FileInputStream;
 import java.io.IOException;
 import com.google.common.collect.Lists;
 
@@ -21,8 +22,9 @@ public class FcmConfig {
         try {
             //firebase sdk 업데이트로 GoogleCredentials 생성하는 방법 변경 => createScoped 추가
             GoogleCredentials credentials = GoogleCredentials
-                    .fromStream(new ClassPathResource(secretFileName).getInputStream())
+                    .fromStream(new FileInputStream(secretFileName)) // 파일 시스템 경로로 파일 읽기
                     .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
+
             FirebaseOptions options = new FirebaseOptions.Builder().setCredentials(credentials).build();
 
             // FirebaseApp 초기화 한 번만 이루어지도록 수정
