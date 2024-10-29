e72daec7d0420d167c42522766b6367dec900273
Choi-Moori <90437809+Choi-Moori@users.noreply.github.com>
2024-10-29 22:15:08 +0900

    TEST : 배포 테스트 20차

--- reservation/src/main/java/com/padaks/todaktodak/common/feign/MemberFeignClient.java
+++ reservation/src/main/java/com/padaks/todaktodak/common/feign/MemberFeignClient.java
@@ -1,15 +1,12 @@
-//package com.padaks.todaktodak.common.feign;
-//
-//import com.padaks.todaktodak.common.config.FeignConfig;
-//import com.padaks.todaktodak.common.dto.MemberFeignDto;
-//import org.springframework.cloud.openfeign.FeignClient;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-////k8s배포시에는 url = "http://member-service" 추가
-//@FeignClient(name = "member-service", url = "http://member-service",  configuration = FeignConfig.class)
-//public interface MemberFeignClient {
-////    @GetMapping("/member/get/member")
-////        // member-service에 구현된 경로
-////    MemberFeignDto getMemberEmail();  // MemberPayDto 반환
-//
-//}
+package com.padaks.todaktodak.common.feign;
+
+import com.padaks.todaktodak.common.config.FeignConfig;
+import com.padaks.todaktodak.common.dto.MemberFeignDto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//k8s배포시에는 url = "http://member-service" 추가
+@FeignClient(name = "member-service", url = "http://member-service",  configuration = FeignConfig.class)
+public interface MemberFeignClient {
+
+}
