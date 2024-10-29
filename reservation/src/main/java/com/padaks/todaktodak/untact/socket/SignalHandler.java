31df48937a7742572f8316abeb550aa18126a5ba
milcho0604 <milcho0604@gmail.com>
2024-10-29 21:24:09 +0900

    feat: reservation bean

--- reservation/src/main/java/com/padaks/todaktodak/untact/socket/SignalHandler.java
+++ reservation/src/main/java/com/padaks/todaktodak/untact/socket/SignalHandler.java
@@ -25,6 +25,7 @@ import java.util.Map;
 import java.util.Objects;
 import java.util.Optional;
 
+
 @Component
 public class SignalHandler extends TextWebSocketHandler implements MessageListener {
     @Autowired
