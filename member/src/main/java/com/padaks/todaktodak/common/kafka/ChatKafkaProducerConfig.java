cf332d38d71e8522a084a192590aab9e2aabb486
milcho0604 <milcho0604@gmail.com>
2024-10-30 20:48:18 +0900

    배포 port

--- member/src/main/java/com/padaks/todaktodak/common/kafka/ChatKafkaProducerConfig.java
+++ member/src/main/java/com/padaks/todaktodak/common/kafka/ChatKafkaProducerConfig.java
@@ -1,5 +1,6 @@
 package com.padaks.todaktodak.common.kafka;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
@@ -20,7 +22,12 @@ import java.util.Map;
 public class ChatKafkaProducerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootStrapServer;
-
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServer);
+        return new KafkaAdmin(configs);
+    }
     @Bean(name = "ChatTopic")
     public NewTopic paymentSuccess(){
         return new NewTopic("ChatTopic", 10 , (short) 1);
--- reservation/src/main/java/com/padaks/todaktodak/common/kafka/KafkaProducerConfig.java
+++ reservation/src/main/java/com/padaks/todaktodak/common/kafka/KafkaProducerConfig.java
@@ -1,5 +1,6 @@
 package com.padaks.todaktodak.common.kafka;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringSerializer;
@@ -7,6 +8,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaAdmin;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
 import org.springframework.kafka.support.serializer.JsonSerializer;
@@ -18,6 +20,13 @@ import java.util.Map;
 public class KafkaProducerConfig {
     @Value("${spring.kafka.bootstrap-servers}")
     private String bootStrapServer;
+
+    @Bean
+    public KafkaAdmin kafkaAdmin() {
+        Map<String, Object> configs = new HashMap<>();
+        configs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServer);
+        return new KafkaAdmin(configs);
+    }
     @Bean(name = "paymentSuccessTopic")
     public NewTopic paymentSuccess(){
         return new NewTopic("payment-success", 10 , (short) 1);
